bb97d1f86c084beaedfd7d97fcd1ac68a2367338
Reteamboss <9169479343@mail.ru>
2022-08-03 13:01:19 +0300

    create userinfo

--- userinfo_for_user.py
+++ userinfo_for_user.py
@@ -1,4 +1,4 @@
-PASSWORD = "" #opensky
-LOGIN = "" #opensky
+# PASSWORD = "" #opensky
+# LOGIN = "" #opensky
 API_KEY = "72a9642f62msha40eb2a29b7498dp1c8666jsn86b05775c6ac"
 API_HOST = "ip-geolocation-ipwhois-io.p.rapidapi.com"
